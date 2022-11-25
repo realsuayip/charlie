684074bf87bf675de95ef0e44a7a7ae975e2c9b7
şuayip üzülmez <suayip.541@gmail.com>
2022-11-25 23:41:24 +0300

    Add MongoDB connection boilerplate

---
+++ condb.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+	"os"
+	"time"
+)
+
+var DBName = os.Getenv("DATABASE_NAME")
+var MongoURI = os.Getenv("MONGO_URI")
+
+type MongoInstance struct {
+	Client   *mongo.Client
+	Database *mongo.Database
+}
+
+var mi MongoInstance
+
+func MongoConnect() {
+	serverAPIOptions := options.ServerAPI(options.ServerAPIVersion1)
+	clientOptions := options.Client().ApplyURI(MongoURI).SetServerAPIOptions(serverAPIOptions)
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+
+	defer cancel()
+	client, err := mongo.Connect(ctx, clientOptions)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db := client.Database(DBName)
+	mi = MongoInstance{Client: client, Database: db}
+}
