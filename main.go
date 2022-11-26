05e0946c8018913c36bfa9f7cd2ed32c7ef97005
şuayip üzülmez <suayip.541@gmail.com>
2022-11-26 16:56:26 +0300

    Boostrap the web service

--- main.go
+++ main.go
@@ -1,3 +1,23 @@
 package main
 
-func main() {}
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"log"
+)
+
+func main() {
+	mi := MongoConnect()
+
+	app := fiber.New()
+	app.Use(logger.New())
+
+	// Routes
+	app.Get("/contracts/", mi.GetContracts)
+	app.Get("/contracts/:id", mi.GetContract)
+
+	err := app.Listen(":3000")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
